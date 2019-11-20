6434117da71e9061b50163271b21ec8b303b9a00
Noah Wilder <noahleviwilder@gmail.com>
2019-11-20 11:10:21 -0500

    Initial Commit

---
+++ Git Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git Practice
+//
+//  Created by Noah Wilder on 2019-11-20.
+//  Copyright © 2019 Noah Wilder. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
